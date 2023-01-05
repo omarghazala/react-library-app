7c0dfb41e9cf7e99a3ad1dc630a2bf01919f14da
omarghazala <omarghazala585@gmail.com>
2023-01-05 17:22:25 +0200

    create-book-entity

--- 02-backend/spring-boot-library/src/main/java/com/bookworm/springbootlibrary/entity/Book.java
+++ 02-backend/spring-boot-library/src/main/java/com/bookworm/springbootlibrary/entity/Book.java
@@ -1,2 +1,30 @@
-package com.bookworm.springbootlibrary.entity;public class Book {
+package com.bookworm.springbootlibrary.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "copies")
+    private int copies;
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "img")
+    private String img;
+
 }
