36ae37ca8bd784ece6504c083016b14f4ba96519
omarghazala <omarghazala585@gmail.com>
2023-05-08 18:10:07 +0300

    add search

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import HomePage from './layouts/HomePage/HomePage';
 import Footer from './layouts/NavbarAndFooter/Footer';
 import Navbar from './layouts/NavbarAndFooter/Navbar';
+import SearchBooksPage from './layouts/SearchBooksPage/SearchBooksPage';
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <SearchBooksPage></SearchBooksPage>
       <Footer></Footer>
     </div>
   );
